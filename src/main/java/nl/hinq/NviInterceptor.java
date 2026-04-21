7a4dfa4498940d64019bab217cc77212b3accf0b
Ahmet Melih Aydoğdu <melih.aydogdu@formelio.nl>
2026-04-21 11:12:18 +0200

    nvi interceptor updated

--- src/main/java/nl/hinq/NviInterceptor.java
+++ src/main/java/nl/hinq/NviInterceptor.java
@@ -9,10 +9,13 @@ import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.jena.atlas.lib.InternalErrorException;
-import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.*;
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.dstu3.model.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class NviInterceptor {
@@ -22,47 +25,80 @@ public class NviInterceptor {
 	private static final String ORG_URA = "90000697";
 	private static final String NVI_ENDPOINT = "http://dev-nuts-hackaton-source-nuts-knooppunt.dev-nuts-hackathon-source.svc.cluster.local:8081/nvi";
 	private static final String MITZ_ENDPOINT = "http://dev-nuts-hackaton-source-nuts-knooppunt.dev-nuts-hackathon-source.svc.cluster.local:8081/mitz";
+	private static final FhirContext fhirContextR4 = FhirContext.forR4();
 
-	@Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_CREATED)
-	public void resourceCreated(RequestDetails requestDetails, IBaseResource newResource) {
-		if (newResource instanceof Patient patient) {
-			log.info("New patient created with ID: {}", newResource.getIdElement().getIdPart());
-			Optional<String> optionalBsn = patient.getIdentifier().stream()
-				.filter(id -> BSN_SYSTEM.equals(id.getSystem()))
-				.findAny()
-				.map(Identifier::getValue);
+	@Hook(Pointcut.STORAGE_TRANSACTION_PROCESSING)
+	public void resourceCreated(RequestDetails requestDetails, IBaseBundle newBundle) {
+		log.info("Resource created {}", requestDetails.getFhirContext().newJsonParser().encodeResourceToString(newBundle));
+		if (newBundle instanceof Bundle bundle) {
+			log.info("Processing nvi request for bundle");
+			Optional<Patient> patient = bundle.getEntry().stream()
+				.filter(entry -> entry.getResource() instanceof Patient)
+				.map(entry -> (Patient) entry.getResource())
+				.findFirst();
+
+			Optional<String> optionalBsn = Optional.empty();
+			if (patient.isPresent()) {
+				optionalBsn = patient.get().getIdentifier().stream()
+					.filter(identifier -> identifier.getSystem().equals(BSN_SYSTEM))
+					.map(Identifier::getValue)
+					.findFirst();
+			}
 
 			if (optionalBsn.isEmpty()) {
 				log.warn("Patient does not have a BSN identifier.");
 				return;
 			}
-			
+
+//			Set<String> resources = bundle.getEntry().stream()
+//				.map(Bundle.BundleEntryComponent::getResource)
+//				.map(resource -> resource.getResourceType().name())
+//				.collect(Collectors.toSet());
+//			log.info("Resources in bundle: {}", resources);
+
 			String subscription = SUBSCRIPTION.formatted(optionalBsn.get(), ORG_URA);
 			log.info("Subscription to post: {}", subscription);
 			postSubscription(requestDetails, subscription);
-			log.info("Posted Subscription to Mitz for patient with ID: {}", newResource.getIdElement().getIdPart());
+			log.info("Posted Subscription to Mitz for patient with ID: {}", patient.get().getIdElement().getIdPart());
 
-			String documentReference = DOCUMENT_REFERENCE.formatted(optionalBsn.get(), ORG_URA);
-			log.info("DocumentReference to post: {}", documentReference);
-			postDocumentReference(requestDetails, documentReference);
-			log.info("Posted DocumentReference to NVI for patient with ID: {}", newResource.getIdElement().getIdPart());
-
+			String list = createListResource(optionalBsn.get(), Set.of("MedicationRequest"));
+			log.info("List to post: {}", list);
+			postList(requestDetails, list);
+			log.info("Posted List to NVI for patient with ID: {}", patient.get().getIdElement().getIdPart());
 		}
 	}
 
-	private void postDocumentReference(RequestDetails requestDetails, String docRef) {
+	private String createListResource(String s, Set<String> resources) {
+		org.hl7.fhir.r4.model.ListResource list = new org.hl7.fhir.r4.model.ListResource();
+		list.addExtension()
+			.setUrl("http://minvws.github.io/generiekefuncties-docs/StructureDefinition/nl-gf-localization-custodian")
+			.setValue(new org.hl7.fhir.r4.model.Reference().setIdentifier(new org.hl7.fhir.r4.model.Identifier().setSystem(URA_SYSTEM).setValue(NviInterceptor.ORG_URA)));
+		list.setSubject(new org.hl7.fhir.r4.model.Reference().setIdentifier(new org.hl7.fhir.r4.model.Identifier().setSystem(BSN_SYSTEM).setValue(s)));
+		list.setSource(new org.hl7.fhir.r4.model.Reference().setIdentifier(new org.hl7.fhir.r4.model.Identifier().setSystem("urn:ietf:rfc:3986").setValue("EHR-SYS-2024-998")));
+		list.setSource(new org.hl7.fhir.r4.model.Reference().setIdentifier(new org.hl7.fhir.r4.model.Identifier().setSystem("urn:ietf:rfc:3986").setValue("EHR-SYS-2024-998")).setType("Device"));
+		list.setStatus(org.hl7.fhir.r4.model.ListResource.ListStatus.CURRENT);
+		list.setMode(org.hl7.fhir.r4.model.ListResource.ListMode.WORKING);
+		list.setEmptyReason(new org.hl7.fhir.r4.model.CodeableConcept().addCoding(new org.hl7.fhir.r4.model.Coding().setSystem("http://terminology.hl7.org/CodeSystem/list-empty-reason").setCode("withheld")));
+		List<org.hl7.fhir.r4.model.Coding> coding = list.getCode().getCoding();
+		for (String resource : resources) {
+			coding.add(new org.hl7.fhir.r4.model.Coding().setSystem("http://minvws.github.io/generiekefuncties-docs/CodeSystem/nl-gf-data-categories-cs").setCode(resource));
+		}
+		return fhirContextR4.newJsonParser().encodeResourceToString(list);
+	}
+
+	private void postList(RequestDetails requestDetails, String listResource) {
 		FhirContext context = requestDetails.getFhirContext();
 		var factory = context.getRestfulClientFactory();
 		factory.setServerValidationMode(ServerValidationModeEnum.NEVER);
 		IGenericClient client = factory.newGenericClient(NVI_ENDPOINT);
 		try {
 			client.create()
-				.resource(docRef)
+				.resource(listResource)
 				.withAdditionalHeader("X-Tenant-Id", URA_SYSTEM+"|"+ORG_URA)
 				.withAdditionalHeader("Content-Type", "application/fhir+json")
 				.execute();
 		} catch (Exception e) {
-			String message = "Error posting DocumentReference to NVI: %s".formatted(e.getMessage());
+			String message = "Error posting List to NVI: %s".formatted(e.getMessage());
 			log.error(message);
 			throw new InternalErrorException(message);
 		}
@@ -84,46 +120,6 @@ public class NviInterceptor {
 			throw new InternalErrorException(message);
 		}
 	}
-
-	private static String DOCUMENT_REFERENCE = "{\n" + //
-				"  \"resourceType\" : \"DocumentReference\",\n" + //
-				"  \"meta\" : {\n" + //
-				"    \"profile\" : [\n" + //
-				"      \"http://nuts-foundation.github.io/nl-generic-functions-ig/StructureDefinition/nl-gf-localization-documentreference\"\n" + //
-				"    ]\n" + //
-				"  },\n" + //
-				"  \"status\" : \"current\",\n" + //
-				"  \"type\" : {\n" + //
-				"    \"coding\" : [\n" + //
-				"      {\n" + //
-				"        \"system\" : \"http://loinc.org\",\n" + //
-				"        \"code\" : \"55188-7\",\n" + //
-				"        \"display\" : \"Patient data Document\"\n" + //
-				"      }\n" + //
-				"    ]\n" + //
-				"  },\n" + //
-				"  \"subject\" : {\n" + //
-				"    \"identifier\" : {\n" + //
-				"      \"system\" : \"http://fhir.nl/fhir/NamingSystem/bsn\",\n" + //
-				"      \"value\" : \"%s\"\n" + //
-				"    }\n" + //
-				"  },\n" + //
-				"  \"custodian\" : {\n" + //
-				"    \"identifier\" : {\n" + //
-				"      \"system\" : \"http://fhir.nl/fhir/NamingSystem/ura\",\n" + //
-				"      \"value\" : \"%s\"\n" + //
-				"    }\n" + //
-				"  },\n" + //
-				"  \"content\" : [\n" + //
-				"    {\n" + //
-				"      \"attachment\" : {\n" + //
-				"        \"contentType\" : \"text/plain\",\n" + //
-				"        \"data\" : \"IlRoaXMgRG9jdW1lbnRSZWZlcmVuY2UgZG9lcyBub3QgcG9pbnQgdG8gYSBzcGVjaWZpYyBkb2N1bWVudCwgYnV0IHJhdGhlciB0byBkb2N1bWVudHMgb3IgZGF0YSBpbiBnZW5lcmFsIGF0IHRoZSBjdXN0b2RpYW4gcmVmZXJlbmNlZCBpbiB0aGlzIGluc3RhbmNlLiBUaGlzIGNhbiBiZSB1c2VkIGJ5IGRhdGEgbG9jYWxpemF0aW9uIHNlcnZpY2VzIChhbHNvIGtub3duIGFzIG1lZGljYWwgcmVjb3JkIGxvY2FsaXphdGlvbiBzZXJ2aWNlcykuIg==\",\n" + //
-				"        \"title\" : \"Generic reference to patient data\"\n" + //
-				"      }\n" + //
-				"    }\n" + //
-				"  ]\n" + //
-				"}";
 
 	private static String SUBSCRIPTION = "{\n" + //
 				"    \"resourceType\": \"Subscription\",\n" + //
